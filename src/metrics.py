ab6ea32662c33d1f9240394337ab60afb170d3ab
Oyeyemi Dare Azeez <31821751+oyeyemixisco@users.noreply.github.com>
2026-09-01 11:07:16 +0200

    Refactor imports for drawing utilities and styles

--- src/metrics.py
+++ src/metrics.py
@@ -5,11 +5,10 @@ import mediapipe as mp
 from PIL import Image
 
 import uuid
-from mediapipe.python.solutions import drawing_utils as mp_drawing
-from mediapipe.python.solutions import drawing_styles as mp_drawing_styles
-
 
 mp_face_mesh = mp.solutions.face_mesh
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 
 
 # -----------------------------
